9fe0f853b7a2854c3b6a463e23064f80f05851dc
LeoTmr <leonardotrotta32@gmail.com>
2021-07-14 00:16:56 -0300

    Conta Especial

--- GenerationBancoPOO/src/contas/ContaEspecial.java
+++ GenerationBancoPOO/src/contas/ContaEspecial.java
@@ -1,8 +1,8 @@
 package contas;
 
-public class ContaEspecial extends Conta{
+public class ContaEspecial extends Conta {
 	private double limiteEspecial = 1000.0;
-	
+
 	public ContaEspecial(String numeroConta, String nomeCliente) {
 		super(numeroConta, nomeCliente);
 		// TODO Auto-generated constructor stub
@@ -15,24 +15,37 @@ public class ContaEspecial extends Conta{
 
 	public double getlimiteEspecial() {
 		return limiteEspecial;
-	}	
-	
-		public void saldoInsuficiente (double valor) {
-			System.out.println("Saldo em conta insuficiente para realizar a operação.");
-			valor = 0.0;
-		
-			if(valor <= getSaldo() + getlimiteEspecial()) {
-				System.out.println("Utilizando " + (valor - getSaldo()) + " do limite especial.");
-			
-				if(valor > 0) {
-					limiteEspecial -=(valor - getSaldo());
-				}
-				super.debito(getSaldo());
-		}
-			else {
-				System.out.println("Seu limite é insuficiente");
-			}
-			System.out.println("Seu limite atual é R$: " + getlimiteEspecial());
 	}
 
+	@Override
+		public void debito (double valor) {
+			if (valor <= 0) {
+				System.out.println("Valor inválido");
+			} else if (valor <= saldo) {
+			 System.out.println("");
+			
+			 double saldo; 
+			 saldo = super.getSaldo();
+			 
+			 	saldo -= valor;
+			 	super.debito(valor);
+				
+				
+			} 
+			else if(valor <= getSaldo() + getlimiteEspecial()) {
+				System.out.println("Saldo em conta insuficiente para realizar a operação.");
+				System.out.println("Utilizando " + (valor - getSaldo()) + " do limite especial.");
+				if(valor > 0)
+				{
+					limiteEspecial -=(valor - getSaldo());
+					super.debito(getSaldo());	
+			}
+				else {
+					System.out.println("Seu limite é insuficiente");
+				}
+			}
+			System.out.println("Seu limite atual é R$: " + getlimiteEspecial());
+			realizarOperacao();
+			}
+			
 }
